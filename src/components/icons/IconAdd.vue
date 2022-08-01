2001b638926ca5a907e11c921cae2f8f8bbde2db
Brett Yoncak <brett.yoncak@PIPHMAC012.local>
2022-08-01 09:10:36 -0500

    A new beginning

---
+++ src/components/icons/IconAdd.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="56"
+    height="56"
+    viewBox="0 0 56 56"
+    fill="none"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M30.8 0H25.2V25.2H0V30.8L25.2 30.8V56H30.8V30.8L56 30.8V25.2H30.8V0Z"
+      fill="url(#paint0_linear_1_16)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_1_16"
+        x1="28"
+        y1="8.34465e-07"
+        x2="56"
+        y2="56"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#F3451E" />
+        <stop offset="0.505208" stop-color="#FFDC23" />
+        <stop offset="1" stop-color="#3AE04B" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import IconAdd from "../components/icons/IconAdd.vue";
+
+
+</script>
+
+<template>
+  <div class="todo-container">
+    <IconAdd />
+  </div>
+</template>
+
+<style scoped>
+.todo-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-sizing: border-box;
+
+  position: absolute;
+  width: 128px;
+  height: 128px;
+
+  border-radius: 100%;
+  background: #1f1f1f;
+  border: 1px solid #565656;
+  box-shadow: -4px 8px 16px rgba(0, 0, 0, 0.56);
+}
+</style>
